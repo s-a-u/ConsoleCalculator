2415078a8eb1ed5976b25653aafcdf0992f5e0ad
s-a-u <s-a-u@i.ua>
2022-03-04 15:31:49 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double firstValue, secondValue;
+                string action;
+                try {
+                    Console.WriteLine("Введите число 1");
+                    firstValue = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите число 2");
+                    secondValue = double.Parse(Console.ReadLine());
+                }
+                catch(Exception)
+                { Console.WriteLine("Нечисловое значение");
+                    Console.ReadLine();
+                    continue;
+                }
+               
+
+                Console.WriteLine("Выберите операцию: '+' '-' '*' '/'");
+
+                action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "+":
+                        Console.WriteLine(firstValue + secondValue);
+                        break;
+                    case "-":
+                        Console.WriteLine(firstValue - secondValue);
+                        break;
+                    case "*":
+                        Console.WriteLine(firstValue * secondValue);
+                        break;
+                    case "/":
+                        if (secondValue == 0)
+                            Console.WriteLine(0);
+                        else
+                            Console.WriteLine(firstValue / secondValue);
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка ! Неизвестное действие!");
+                        break;
+                }
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
